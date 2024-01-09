4cdb38b2df7e1f26b0df3a737d3931af60f04183
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2024-01-09 21:04:00 +0300

    Update categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,39 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
+  def index; end
+
+  def home
+    @categories = current_user.categories
+  end
+
+  def new
+    @category = Category.new
+    @choices = [
+      ['🛒'], ['🍽️'],
+      ['⚽'], ['📐'],
+      ['🚗'], ['🛋️'],
+      ['🍔'], ['⚕️'],
+      ['🎮'], ['💻'],
+      ['🌿'], ['🎨'],
+      ['💼'], ['📱']
+    ]
+  end
+
+  def create
+    @user= current_user
+    @category = @user.categories.new(categories_params)
+    @category.user = @user
+    if @category.save
+      redirect_to home_categories_path, notice: 'Category was successfully created'
+    else
+      redirect_to new_category_path, alert: 'Can\'t create a new category'
+    end
+  end
+
+  private
+
+  def categories_params
+    params.require(:category).permit(:name, :icon).merge(user: current_user)
+  end
 end
