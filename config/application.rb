796618a15fd093f8d9c5dce0f2bffad576f2cfc7
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2024-01-10 20:08:03 +0300

    Add last styles

--- config/application.rb
+++ config/application.rb
@@ -6,23 +6,11 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module BudgetApp
+module BudgetBeam
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.0
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
-    
-    config.assets.enabled = true
-
-    config.assets.debug = true
-
-    config.assets.compile = false
-  
-    config.action_controller.raise_on_missing_callback_actions = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
