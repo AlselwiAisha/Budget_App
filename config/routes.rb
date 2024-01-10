4df5096a3f933133be3bfe8a195e608e733ff98f
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2024-01-10 12:08:49 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  authenticated :user do
+    root 'categories#home', as: :authenticated_root
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  unauthenticated do
+    root 'categories#index', as: :unauthenticated_root
+  end
+
+   devise_scope :user do
+    get 'users/sign_out', to: 'devise/sessions#destroy'
+  end
+
+  resources :categories, only: %i[index home new create destroy] do
+    get :home, on: :collection
+    resources :transacts, only: %i[index new create]
+  end
 end
